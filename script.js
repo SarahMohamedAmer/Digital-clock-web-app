9f746c3ccbcdf1cfcc7422456e443d24b4ed1524
Sarah Amer <97125596+SarahMohamedAmer@users.noreply.github.com>
2023-06-04 16:14:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+
+function showTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    
+
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    else if(h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+
+  
+    
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("clock").innerText = time;
+
+    setTimeout(showTime, 1000);
+}
+
+
+
+showTime();
+
+// Change background image based on time of day
+var body = document.querySelector('body');
+var currentHour = new Date().getHours();
+
+if (currentHour >= 18 || currentHour < 6) {
+  body.classList.add('night');
+}
+else {
+  body.classList.remove('night');
+}
+
+
+
+n
